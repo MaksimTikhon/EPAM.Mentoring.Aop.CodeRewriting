6c72086908d57513aec0a4bcc430e58156b027f3
Maksimtikhon <tikhon.maxim@gmail.com>
2016-09-25 20:33:45 +0300

    Implemented code rewriting using PostSharp

---
+++ EPAM.Mentoring.Aop.CodeRewriting/SimpleCalculator/CalculatorHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Logger;
+
+namespace SimpleCalculator
+{
+	[LoggerPostSharpAspect]
+	public class CalculatorHelper
+	{
+		private const string AvailableOperators = "+-";
+
+		public ParsedInput Parse(string expressin)
+		{
+			char[] delimiters = { ' ' };
+			var operands = expressin.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+			int firstArgument;
+			int secondArgument;
+
+			if (operands.Length == 3 && int.TryParse(operands[0], out firstArgument)
+					&& int.TryParse(operands[2], out secondArgument) && AvailableOperators.Contains(operands[1]))
+			{
+				return new ParsedInput
+				{
+					FirstArgument = firstArgument,
+					SecondArgument = secondArgument,
+					Operator = operands[1],
+					Success = true
+				};
+			}
+
+			return new ParsedInput
+			{
+				Success = false
+			};
+		}
+
+		public long Sum(int firstArgument, int secondArgument)
+		{
+			return firstArgument + secondArgument;
+		}
+
+		public long Subtract(int firstArgument, int secondArgument)
+		{
+			return firstArgument - secondArgument;
+		}
+	}
+}
---
+++ EPAM.Mentoring.Aop.CodeRewriting/SimpleCalculator/Caluclator.cs
@@ -0,0 +1,46 @@
+﻿using Logger;
+
+namespace SimpleCalculator
+{
+	[LoggerPostSharpAspect]
+	public class Caluclator
+	{
+		private readonly CalculatorHelper _calculatorHelper;
+
+		public Caluclator()
+		{
+			_calculatorHelper = new CalculatorHelper();
+		}
+
+		public CalculatedResult Process(string expressin)
+		{
+			var parsedInput = _calculatorHelper.Parse(expressin);
+			if (parsedInput.Success)
+			{
+				long result = 0;
+
+				switch (parsedInput.Operator)
+				{
+					case "+":
+						result = _calculatorHelper.Sum(parsedInput.FirstArgument, parsedInput.SecondArgument);
+						break;
+
+					case "-":
+						result = _calculatorHelper.Subtract(parsedInput.FirstArgument, parsedInput.SecondArgument);
+						break;
+				}
+
+				return new CalculatedResult
+				{
+					Result = result,
+					Success = true
+				};
+			}
+
+			return new CalculatedResult
+			{
+				Success = false
+			};
+		}
+	}
+}
